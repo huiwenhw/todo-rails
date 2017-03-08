a8a32c1959404695ae0e90ec1de399d8f3b5bcc9
Hui Wen <huiwen_28@hotmail.com>
2017-03-09 01:36:26 +0800

    Mark as complete done

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create] #dont want it on the create method
 
 	# After we create a todoitem under a todolist 
 	# want to redirect to the todo_list path 
@@ -9,7 +10,6 @@ class TodoItemsController < ApplicationController
 	end
 
 	def destroy
-		@todo_item = @todo_list.todo_items.find(params[:id])
 		if @todo_item.destroy
 			flash[:success] = "Todo List item was deleted"
 		else
@@ -19,12 +19,21 @@ class TodoItemsController < ApplicationController
 		redirect_to @todo_list
 	end
 
+	def complete 
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "Todo item completed"
+	end
+
 	# create private method. TodoList references the model 
 	private 
 
 	def set_todo_list
 		@todo_list = TodoList.find(params[:todo_list_id])
 	end 
+
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
+	end
 
 	def todo_item_params
 		params[:todo_item].permit(:content)
